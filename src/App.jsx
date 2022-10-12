685b81c5a51f9e31f181a50089cdec13e44d64e8
IbrohimRasulov <ibrohim05062000@gmail.com>
2022-10-13 04:12:39 +0500

    Add packages and libraries

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-function App() {
-  return (
-    <div className="App">
+import Navbar from "./components/Layout/Navbar.jsx";
 
-    </div>
-  )
+const App = () => {
+    return (
+        <div className="App">
+            <Navbar/>
+
+            <h2 className="has-text-custom">Hello World</h2>
+        </div>
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/Layout/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div className="button">
+            hello
+            <div></div>
+            <div>
+
+            </div>
+
+        </div>
+    );
+}
+
+export default Navbar;
